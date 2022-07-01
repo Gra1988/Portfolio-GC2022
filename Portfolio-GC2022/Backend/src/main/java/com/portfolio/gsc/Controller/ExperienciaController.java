37adbc6efd8d36c23a5627ab68d3c13c1f71d135
Gra1988 <grasil1988@gmail.com>
2022-06-30 23:00:30 -0300

    Creación de Entity Experiencia en Backend

---
+++ Portfolio-GC2022/Backend/src/main/java/com/portfolio/gsc/Controller/ExperienciaController.java
@@ -0,0 +1,56 @@
+package com.portfolio.gsc.Controller;
+
+import com.portfolio.gsc.Entity.Experiencia;
+import com.portfolio.gsc.Interface.IExperienciaService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExperienciaController {
+ @Autowired IExperienciaService iexperienciaService;
+ 
+ @GetMapping("experiencias/traer")
+ public List<Experiencia> getExperiencia(){
+ return iexperienciaService.getExperiencia();
+ }
+ 
+ @PostMapping("/experiencias/crear")
+ public String createExperiencia(@RequestBody Experiencia experiencia){
+ iexperienciaService.saveExperiencia(experiencia);
+ return "La experiencia fue creada exitosamente";
+ }
+ 
+ @DeleteMapping("/experiencias/borrar/{id}")
+ public String deleteExperiencia(@PathVariable Long id){
+     iexperienciaService.deleteExperiencia(id);
+     return "La experiencia fue eliminada exitosamente";
+ }
+ //URL:PUERTO/experiencias/editar/(id)/empresa & fecha inicio & fecha fin & cargo & descripción
+ @PutMapping("/experiencias/editar/{id}")
+ public Experiencia editExperiencia(@PathVariable Long id,
+                                         @RequestParam("empresa") String nuevoEmpresa,
+                                         @RequestParam("fecha_inicio") String nuevoFecha_inicio,
+                                         @RequestParam("fecha_fin") String nuevoFecha_fin,
+                                         @RequestParam("cargo") String nuevoCargo,
+                                         @RequestParam("descripción") String nuevoDescripción){
+     
+     Experiencia experiencia = iexperienciaService.findExperiencia(id);
+     
+     experiencia.setEmpresa(nuevoEmpresa);
+     experiencia.setFecha_inicio(nuevoFecha_inicio);
+     experiencia.setFecha_fin(nuevoFecha_fin);
+     experiencia.setCargo(nuevoCargo);
+     experiencia.setDescripción(nuevoDescripción);
+     
+     iexperienciaService.saveExperiencia(experiencia);
+     return experiencia;
+ }
+}
---
+++ Portfolio-GC2022/Backend/src/main/java/com/portfolio/gsc/Entity/Experiencia.java
@@ -0,0 +1,34 @@
+package com.portfolio.gsc.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@Entity
+public class Experiencia {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    @Size(min = 1, max = 500, message = "no cumple con la longitud")
+    private String empresa;
+    @NotNull
+    @Size(min = 1, max = 500, message = "no cumple con la longitud")
+    private String fecha_inicio;
+    @NotNull
+    @Size(min = 1, max = 500, message = "no cumple con la longitud")
+    private String fecha_fin;
+    @NotNull
+    @Size(min = 1, max = 500, message = "no cumple con la longitud")
+    private String cargo;
+    @NotNull
+    @Size(min = 1, max = 500, message = "no cumple con la longitud")
+    private String descripción;
+    }
