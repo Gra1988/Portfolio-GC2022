1dde1a16fa540f4135ec82673d78e949d9dbe828
Gra1988 <95895591+Gra1988@users.noreply.github.com>
2022-07-31 21:10:40 -0300

    base y back deployed

--- Portfolio-GC2022/Backend/src/main/java/com/portfolio/gsc/GscApplication.java
+++ Portfolio-GC2022/Backend/src/main/java/com/portfolio/gsc/GscApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GscApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GscApplication.class, args);
-	}
+public static void main(String[] args) {
+SpringApplication.run(GscApplication.class, args);
+}
 
 }
